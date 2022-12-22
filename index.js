7e4bab7ac23044176ddbc7d44dbdba39905d9d70
nurulcse7 <nurul.cse7@gmail.com>
2022-12-22 20:47:46 +0600

    added jwt

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-// middle wares
+// middlewares
 app.use(cors());
 app.use(express.json());
 
@@ -23,6 +23,23 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+// function verifyJWT(req, res, next){
+//     const authHeader = req.headers.authorization;
+
+//     if(!authHeader){
+//         return res.status(401).send({message: 'unauthorized access'});
+//     }
+//     const token = authHeader.split(' ')[1];
+
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+//         if(err){
+//             return res.status(403).send({message: 'Forbidden access'});
+//         }
+//         req.decoded = decoded;
+//         next();
+//     })
+// }
 
 async function run() {
 	try {
@@ -84,6 +101,13 @@ async function run() {
 		const result = await orderCollection.updateOne(query, updatedDoc);
 		res.send(result);
 	  });
+
+	  // JWT 69-5
+	  app.post('/jwt', (req, res) =>{
+		const user = req.body;
+		const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '7d'})
+		res.send({token})
+	})
 
 	} finally {
 	}
