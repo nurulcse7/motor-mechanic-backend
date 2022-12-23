3ac801f730ec0e0c0245d2dafb240112c34f3d38
nurulcse7 <nurul.cse7@gmail.com>
2022-12-23 11:05:44 +0600

    SSL 01 init

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+// SSLCommerz
+const SSLCommerzPayment = require('sslcommerz-lts')
+const store_id = process.env.NODE_SSL_STORE_ID
+const store_passwd = process.env.NODE_SSL_STORE_PASS
+const is_live = false //true for live, false for sandbox
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -70,12 +75,61 @@ async function run() {
       const service = await serviceCollection.findOne(query);
       res.send(service);
     });
-    // create a order when user submit 67-5 // verify 69-8
+
+    // ============= SSLCommerz Payment Gateway ================
+    // create a order when user submit 67-5 // verify 69-8 // SSL 0-5 
     app.post('/orders', verifyJWT, async (req, res) => {
       const order = req.body;
-      const result = await orderCollection.insertOne(order);
-      res.send(result);
-    });
+      // const result = await orderCollection.insertOne(order);
+      // res.send(result);
+      // const { service, email, address } = order;
+      // if (!service || !email || !address) {
+      //   return res.send({ error: 'Please provide all the information' });
+      // }
+      const orderedService = await serviceCollection.findOne({_id: ObjectId(order.service), });
+      console.log(orderedService);
+      //  res.send(orderedService);
+      const transactionId = new ObjectId().toString();
+      const data = {
+        total_amount: orderedService.price,
+        currency: order.currency,
+        tran_id: transactionId, // use unique tran_id for each api call
+        success_url: 'http://localhost:3030/success',
+        fail_url: 'http://localhost:3030/fail',
+        cancel_url: 'http://localhost:3030/cancel',
+        ipn_url: 'http://localhost:3030/ipn',
+        shipping_method: 'Courier',
+        product_name: 'Computer.',
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: order.customer,
+        cus_email: order.email,
+        cus_add1: order.address,
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: order.postcode,
+        ship_country: 'Bangladesh',
+    };
+    console.log(data)
+    res.send(data)
+    // const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+    // sslcz.init(data).then(apiResponse => {
+    //     // Redirect the user to payment gateway
+    //     let GatewayPageURL = apiResponse.GatewayPageURL
+    //     res.redirect(GatewayPageURL)
+    //     console.log('Redirecting to: ', GatewayPageURL)
+    // });
+  });
 
     // orders api 67-6 // get orders from database // verify 69-7
     app.get('/orders', verifyJWT, async (req, res) => {
